efc47dd4496f10e23d7ffae35cc5e48c8bfd663e
rei-k <37492997+rei-k@users.noreply.github.com>
2024-01-26 14:23:29 +0900

    Delete javawork directory

--- javawork/基礎構文（変数、for文、if文）/4/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.IntStream;
-
-public class Main {
-    public static void main(String[] args) {
-        IntStream.rangeClosed(1, 100)
-                 .filter(n -> n % 2 == 0)
-                 .forEach(n -> System.out.print(n + " "));
-    }
-}
